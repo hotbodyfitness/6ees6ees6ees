0b22fb756ff8f4a7f84229af8cbd17d728289a45
Zack Miller <hotbodyfitness@gmail.com>
2019-05-28 10:52:17 -0700

    Finished sprint

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,10 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee {
+  constructor (food, eat, color) {
+    super(food, eat, color);
+    this.age = 10;
+    this.job = 'make honey';
+    this.honeyPot = 0;
+    this.makeHoney = () => this.honeyPot++; // not sure if it's ok to return this value
+    this.giveHoney = () => this.honeyPot--; // not sure if it's ok to return this value
+  }
 };
--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,12 @@
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends ForagerBee {
+  constructor(food, eat) {
+    super(food, eat);
+    this.color = 'grey';
+    this.age = 40;
+    this.job = 'gamble';
+    this.canFly = false;
+    this.treasureChest = [];
+    this.forage = () => 'I am too old, let me play cards instead';
+    this.gamble = treasure => this.treasureChest.push(treasure);
+  }
 };
